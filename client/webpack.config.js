9cd47517f295030a6b58322ecaf2399a9b084dea
dot <adigun.jolly@gmail.com>
2026-04-09 17:53:25 -0500

    fixed config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,42 +1,49 @@
 const webpack = require("webpack");
-const path = require("path"); 
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode:"development",
-    entry:"./src/index.js",
-    output:{
-        path:path.resolve(__dirname, "dist"),
-        publicPath:"/dist/",
-        filename:"main.js",
-        assetModuleFilename:"[name][ext]"
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",          
+        filename: "main.js",
+        assetModuleFilename: "[name][ext]"
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                loader:"babel-loader",
-                options:{
-                    presets:["@babel/preset-env", "@babel/preset-react"]
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
                 }
             },
             {
-                test:/\.(scss|css)$/,
-                use:["style-loader","css-loader", "sass-loader"]
+                test: /\.(scss|css)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
             {
-                test:/\.(png|jpg|jpeg|svg)$/,
-                type:"assert/resource"
+                test: /\.(png|jpg|jpeg|svg)$/,
+                type: "asset/resource"   // fixed typo
             }
         ]
     },
-    resolve:{
-        extensions:["*", ".js", ".jsx"]
+    resolve: {
+        extensions: [".js", ".jsx"]
     },
-    devServer:{
-        port:3000,
-        historyApiFallback:true,
-        host:"localhost"
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        host: "localhost",
+        hot: true
     },
-    plugins:[new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html")
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
